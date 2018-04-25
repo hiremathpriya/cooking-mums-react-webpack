acf3681b70cf2939639465be21f16f95af79f197
Priya Hiremath <priyahrish18@gmail.com>
2018-04-25 17:13:37 +1200

    Comment out cors option

--- server/server.js
+++ server/server.js
@@ -4,21 +4,25 @@ const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const registrations = require('./routes/registrations.js')
+const registrations = require('./routes/registrations')
+const images = require('./routes/images')
 
 const server = express()
 
+/*
 const corsOptions = {
     origin: 'http://localhost:8080',
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS']
 }
+*/
 
 server.use(bodyParser.json())
 
-server.use(cors(corsOptions))
+//server.use(cors(corsOptions))
 server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/api/v1/registrations', registrations)
+server.use('/api/v1/images', images)
 
 module.exports = server
